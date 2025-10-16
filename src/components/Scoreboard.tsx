ebf03fb77b3c9f078b1124ec1394bb3d52133287
nlojkine <46084083+S-poony@users.noreply.github.com>
2025-10-16 16:48:35 +0700

    added score board back

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -13,8 +13,8 @@ export function Scoreboard({ score, pesticideSprayCount }: ScoreboardProps) {
         </div>
       <CardContent className="space-y-4">
         <div className="flex justify-between">
-          <p className="font-medium">Score:</p>
-          <p className="text-lg font-bold">{score.toFixed(0)}</p>
+          <p className="font-medium">Yield:</p>
+          <p className="text-lg font-bold">{score.toFixed(2)}</p>
         </div>
         <div className="flex justify-between">
           <p className="font-medium">Pesticide Sprays:</p>
